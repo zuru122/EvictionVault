807228023993a75c4977e07cfc27f4a1ee89c60d
Justice <onyekaozuru122@gmail.com>
2026-03-09 18:03:57 +0100

    create an interface

---
+++ src/interface/IEVictionVault.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IEvictionVault {
+    event Deposit(address indexed depositor, uint256 amount);
+    event Withdrawal(address indexed withdrawer, uint256 amount);
+    event Submission(uint256 indexed txId);
+    event Confirmation(uint256 indexed txId, address indexed owner);
+    event Execution(uint256 indexed txId);
+    event MerkleRootSet(bytes32 indexed newRoot);
+    event Claim(address indexed claimant, uint256 amount);
+
+    function deposit() external payable;
+    function withdraw(uint256 amount) external;
+    function submitTransaction(address to, uint256 value, bytes calldata data) external;
+    function confirmTransaction(uint256 txId) external;
+    function executeTransaction(uint256 txId) external;
+    function claim(bytes32[] calldata proof, uint256 amount) external;
+    function emergencyWithdrawAll() external;
+}
